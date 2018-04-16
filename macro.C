ba302b6b140d5a7364f9a7e87fdf9415bc33ee99
mmarchegiani <matmarcheg@gmail.com>
2018-04-16 02:34:45 +0200

    Reorganizing

--- macro.C
+++
@@ -1,8 +0,0 @@
-void macro(int entries) {
-	//gRoot->Reset();
-	TFile *_file0 = TFile::Open("latino_WpWmJJ_EWK.root");
-	if(entries < 0)
-		latino->Process("mios.C");			//eseguo il selettore su tutt gli eventi del TTree
-	else
-		latino->Process("mios.C", "", entries);		//eseguo il selettore solamente sui primi "entries" eventi del TTree
-}
