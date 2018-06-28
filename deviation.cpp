11056ff3772279a6c6e55bb18e072fa5a84918ad
mmarchegiani <matmarcheg@gmail.com>
2018-06-28 16:15:43 +0200

    Ultimate deviation + overtrain plots

--- deviation.cpp
+++ deviation.cpp
@@ -9,6 +9,7 @@ g++ deviation.cpp -o deviation.o `root-config --cflags --glibs`
 #include <TMultiGraph.h>
 #include <TGraph.h>
 #include <TGraphErrors.h>
+#include <TAxis.h>
 #include <TCanvas.h>
 #include <TApplication.h>
 #include <TStyle.h>
@@ -19,51 +20,125 @@ g++ deviation.cpp -o deviation.o `root-config --cflags --glibs`
 
 int main(int argc, char **argv) {
 	if(argc < 2) {
-		std::cout << "Errore: inserire file sorgente come argomento." << std::endl;
+		std::cout << "Errore: inserire file sorgente come primo argomento." << std::endl;
 		return 1;
 	}
 
+	if(argc < 3) {
+		std::cout << "Errore: inserire opzione come secondo argomento." << std::endl;
+		return 1;
+	}
+
+	int nColoumns = 4;
+	TString option(argv[2]);
+	if(option == "overtraining")	nColoumns = 6;
+
 	const int n = 50;
-	//char *method[n];
-	double shrink[n], mean[n], rms[n];
+	double par[n], mean[n], rms[n];
+	double mean1[n], rms1[n];
+	double devmax = 0., rmsmax = 0.;
 
 	std::ifstream infile(argv[1]);
 	int j = 0;
 	char buffer[20];
-	infile >> buffer >> buffer >> buffer >> buffer;
+
+	for(int i = 0; i < nColoumns; i++) {
+		infile >> buffer;
+	}
+	
 	while(infile.good()) {
-		//infile >> method[j] >> shrink[j] >> mean[j] >> rms[j];
-		infile >> buffer >> shrink[j] >> mean[j] >> rms[j];
+		if(nColoumns == 4) {
+			infile >> buffer >> par[j] >> mean[j] >> rms[j];
+			mean1[j] = 0.;
+			rms1[j] = 0.;
+		}
+		if(nColoumns == 6)
+			infile >> buffer >> par[j] >> mean[j] >> rms[j] >> mean1[j] >> rms1[j];
+
+		if(fabs(mean[j]) > devmax)
+			devmax = fabs(mean[j]);
+		if(fabs(mean1[j]) > devmax)
+			devmax = fabs(mean1[j]);
+		if(rms[j] > rmsmax)
+			rmsmax = rms[j];
+		if(rms1[j] > rmsmax)
+			rmsmax = rms1[j];
 		j++;
 	}
 	infile.close();
 	int npoints = j;
 
+	TString TrainName(buffer);
+	if(TrainName == "NT")		TrainName = "NTrees";
+	if(TrainName == "SK")		TrainName = "Shrinkage";
+	if(TrainName == "F")		TrainName = "Fraction";
+	if(TrainName == "MD")		TrainName = "MaxDepth";
+
 	TApplication* Grafica = new TApplication("Grafica", 0, NULL);
-	TGraph *deviation = new TGraph(npoints-1, shrink, mean);
-	TGraph *RMS = new TGraph(npoints-1, shrink, rms);
+	TGraph *deviation = new TGraph(npoints-1, par, mean);
+	TGraph *RMS = new TGraph(npoints-1, par, rms);
+	TGraph *deviation1;
+	TGraph *RMS1;
+   	TLegend *legend = new TLegend(0.78, 0.60, 0.98, 0.75);
 
-	deviation->SetTitle("deviation vs Shrinkage; Shrinkage; Deviation");
-	deviation->SetMarkerColor(kBlue);
-	deviation->SetLineColor(kBlue);
+	if(nColoumns == 6) {
+		deviation1 = new TGraph(npoints-1, par, mean1);
+		RMS1 = new TGraph(npoints-1, par, rms1);
+
+		deviation1->SetMarkerColor(kBlue);
+		deviation1->SetLineColor(kBlue);
+		deviation1->SetMarkerStyle(7);
+		deviation1->SetMarkerSize(5);
+
+		RMS1->SetMarkerColor(kBlue);
+		RMS1->SetLineColor(kBlue);
+		RMS1->SetMarkerStyle(7);
+		RMS1->SetMarkerSize(5);
+	}
+
+	deviation->SetMarkerColor(kRed);
+	deviation->SetLineColor(kRed);
 	deviation->SetMarkerStyle(7);
-	deviation->SetMarkerSize(5);
+	deviation->SetMarkerSize(5);	
 
-	RMS->SetTitle("RMS vs Shrinkage; Shrinkage; RMS");
 	RMS->SetMarkerColor(kRed);
 	RMS->SetLineColor(kRed);
 	RMS->SetMarkerStyle(7);
 	RMS->SetMarkerSize(5);
 
+	TMultiGraph *gr_dev = new TMultiGraph();
+	TMultiGraph *gr_rms = new TMultiGraph();
+	gr_dev->Add(deviation);
+	gr_dev->SetTitle("deviation vs " + TrainName + "; "+ TrainName + "; Deviation");
+	gr_rms->Add(RMS);
+	gr_rms->SetTitle("RMS vs " + TrainName + "; "+ TrainName + "; RMS");
+
+	if(nColoumns == 6) {	
+		gr_dev->Add(deviation1);
+		gr_rms->Add(RMS1);
+	}
+
+
 	gStyle->SetOptFit(1111);
 
-	TCanvas *c1 = new TCanvas("Shrinkage","Shrinkage",800,600);
+	TCanvas *c1 = new TCanvas(TrainName,TrainName,800,600);
 	c1->Divide(2,1);
 	c1->cd(1);
-	deviation->Draw("ape");
+	gr_dev->Draw("ape");
+	
 	c1->cd(2);
-	RMS->Draw("ape");
-	c1->Print("Shrinkage/deviation.png");
+	gr_rms->Draw("ape");
+
+	if(nColoumns == 6) {
+		legend->AddEntry(RMS, "Train", "p");
+   		legend->AddEntry(RMS1, "Test", "p");
+   		legend->Draw();
+	}
+
+	if(option == "overtraining")
+		c1->Print(TrainName + "/overtrain.png");
+	else
+		c1->Print(TrainName + "/deviation.png");
 
 	Grafica->Run();
 	return 0;
